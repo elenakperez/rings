1a92e83afde2524b2f32ae82f9ea239575547cae
Elena K. Perez <66798346+elenakperez@users.noreply.github.com>
2023-08-16 16:54:27 -0400

    added map plot section to manuscript nb

--- utils/rings_plot_utils.py
+++ utils/rings_plot_utils.py
@@ -1,8 +1,8 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Utilities to plot META eddy trajectories & statistical analyses
 
-    1)  : 
-    2)  : 
+    1)  all_eddy_tracks_map : returns a map of NWA with eddy tracks for the whole period
+    2)                      : 
     3)               :
     4)               :
     5)               :
@@ -42,3 +42,67 @@ sys.path.append("..")
 from utils.eddy_data_utils import *
 
 #-------------------------------------------------------------------------------------------------------------------------------
+# )
+def all_eddy_tracks_map(eddy_wcr_df, eddy_ccr_df, bathy, title, fig_quality):
+    # gangopadhyay census bounds
+    x_bnds = [-80,-55] # lon, NWA: [-82,-48]
+    y_bnds = [30,46] # lat, NWA: [24,53]
+    
+    # map projection
+    proj = ccrs.PlateCarree()
+
+    # create figure 
+    fig,ax = plt.subplots(subplot_kw = dict(projection=proj),figsize=(10,7))
+    fig.set_dpi(fig_quality)
+    fig.suptitle(title, fontsize=20, y=0.925)
+
+    ## WCR-like eddies ##
+    for i in np.array(eddy_wcr_df['track'].unique()):
+        eddy = eddy_wcr_df[eddy_wcr_df['track']==i]
+        ax.plot(eddy['longitude'],eddy['latitude'],color='#F42119') # anticyclonic
+    
+    ## CCR-like eddies ##     
+    for i in np.array(eddy_ccr_df['track'].unique()):
+        eddy = eddy_ccr_df[eddy_ccr_df['track']==i]
+        ax.plot(eddy['longitude'],eddy['latitude'],color='#114DCE') # cyclonic
+
+    # axes formatting
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+    ax.contour(bathy.lon,bathy.lat,bathy.z,levels=[-4000,-1000,-100],colors='gray') #,levels=[-4000,-1000,-100]
+    ax.coastlines(resolution='50m',color='gray')
+    ax.set_extent([x_bnds[0],x_bnds[1],y_bnds[0],y_bnds[1]], crs=ccrs.PlateCarree())
+    ax.add_feature(cartopy.feature.LAND, color='lightgray')  
+
+    # custom legend
+    from matplotlib.lines import Line2D
+    custom_lines = [Line2D([0], [0], color='#F42119', lw=4), Line2D([0], [0], color='#114DCE', lw=4)]
+    ax.legend(custom_lines, ['WCR-like', 'CCR-like'], loc='upper left')
+
+    # gridlines
+    gl = ax.gridlines(crs=proj,draw_labels=True)
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+    gl.yformatter = LATITUDE_FORMATTER
+    gl.xformatter = LONGITUDE_FORMATTER
+    
+    return fig,ax
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
